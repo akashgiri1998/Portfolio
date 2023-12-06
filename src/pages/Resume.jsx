c41815f0e5101e0c867a9cb8683b94f554b71636
akashgiri1998 <akashgirigiri2@gmail.com>
2023-12-06 23:16:29 +0545

    cv link updated

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,10 +11,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 const resumeLink = `https://akashgiri1998.github.io/Portfolio/src/assets/AkashCV.pdf?raw=true`;
 
 const Resume = () => {
